810fd6a9c7853dfa8811228b559f49ab15b224de
Betrayalovsky <48862068+Betrayalovsky@users.noreply.github.com>
2020-07-13 13:59:04 +0200

    Add files via upload

---
+++ bintod/bintod/bintoD.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace bintod
+{
+    partial class bintoD
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.picSlider = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tipValue = new System.Windows.Forms.ToolTip(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picSlider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.BackColor = System.Drawing.Color.Transparent;
+            this.lblResult.Location = new System.Drawing.Point(121, 11);
+            this.lblResult.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(16, 17);
+            this.lblResult.TabIndex = 7;
+            this.lblResult.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 276);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "100";
+            this.label2.Visible = false;
+            // 
+            // picSlider
+            // 
+            this.picSlider.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picSlider.BackColor = System.Drawing.Color.Transparent;
+            this.picSlider.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picSlider.Location = new System.Drawing.Point(15, 45);
+            this.picSlider.Margin = new System.Windows.Forms.Padding(4);
+            this.picSlider.Name = "picSlider";
+            this.picSlider.Size = new System.Drawing.Size(231, 37);
+            this.picSlider.TabIndex = 4;
+            this.picSlider.TabStop = false;
+            this.picSlider.Paint += new System.Windows.Forms.PaintEventHandler(this.picSlider_Paint);
+            this.picSlider.MouseDown += new System.Windows.Forms.MouseEventHandler(this.picSlider_MouseDown);
+            this.picSlider.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picSlider_MouseMove);
+            this.picSlider.MouseUp += new System.Windows.Forms.MouseEventHandler(this.picSlider_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Select a value";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 276);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "0";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(121, 276);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "yellow";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(191, 276);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "red";
+            this.label5.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(228, 276);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "red";
+            this.label6.Visible = false;
+            // 
+            // bintoD
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.picSlider);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "bintoD";
+            this.Size = new System.Drawing.Size(607, 293);
+            ((System.ComponentModel.ISupportInitialize)(this.picSlider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox picSlider;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolTip tipValue;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ bintod/bintod/bintoD.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bintod
+{
+    public partial class bintoD : UserControl
+    {
+        public object Integer { get; private set; }
+        [Description("Ustawienie wartości maksymalnej suwaka"), Category("Komponent")]
+        public string maxValue
+        {
+            get { return label2.Text; }
+            set { MaximumValue = Int32.Parse(value);
+                label2.Text = value;
+            }
+        }
+        [Description("Ustawienie wartości minimalnej suwaka"), Category("Komponent")]
+        public string minValue
+        {
+            get { return label3.Text; }
+            set
+            {
+                MinimumValue = Int32.Parse(value);
+                label3.Text = value;
+            }
+        }
+        [Description("Zmiana koloru suwaka"), Category("Komponent")]
+        public string colorSLider
+        {
+            get { return label4.Text; }
+            set
+            {
+               
+                label4.Text = value;
+                picSlider.BackColor = Color.FromName(label4.Text);
+            }
+        }
+        [Description("Zmiana koloru suwaka"), Category("Komponent")]
+        public string colorSuwak
+        {
+            get { return label5.Text; }
+            set
+            {
+                label5.Text = value;
+            }
+        }
+        [Description("Zmiana koloru czcionki wyniku"), Category("Komponent")]
+        public string colorText
+        {
+            get { return label6.Text; }
+            set
+            {
+                label6.Text = value;
+                lblResult.ForeColor = Color.FromName(label6.Text);
+            }
+        }
+
+
+        public bintoD()
+        {
+            InitializeComponent();
+            
+        }
+        
+        private float SliderValue = 0.0f;
+
+        
+        int MinimumValue;
+        int MaximumValue;
+
+       
+        private bool MouseIsDown = false;
+        private void picSlider_MouseDown(object sender, MouseEventArgs e)
+        {
+            MouseIsDown = true;
+            SetValue(XtoValue(e.X));
+        }
+        private void picSlider_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!MouseIsDown) return;
+            SetValue(XtoValue(e.X));
+        }
+        private void picSlider_MouseUp(object sender, MouseEventArgs e)
+        {
+            MouseIsDown = false;
+            tipValue.Hide(this);
+
+           
+            lblResult.Text = SliderValue.ToString("0.00");
+        }
+
+        
+        private float XtoValue(int x)
+        {
+            return MinimumValue + (MaximumValue - MinimumValue) *
+                x / (float)(picSlider.ClientSize.Width - 1);
+        }
+
+        
+        private float ValueToX(float value)
+        {
+            return (picSlider.ClientSize.Width - 1) *
+                (value - MinimumValue) / (float)(MaximumValue - MinimumValue);
+        }
+
+        
+        private void picSlider_Paint(object sender, PaintEventArgs e)
+        {
+            
+            
+            float x = ValueToX(SliderValue);
+
+            
+            using (Pen pen = new Pen(Color.FromName(label5.Text), 2))
+            {
+                
+                e.Graphics.DrawLine(pen, x, 0,
+                    x, picSlider.ClientSize.Height);
+            }
+        }
+
+        
+        private void SetValue(float value)
+        {
+            
+            if (value < MinimumValue) value = MinimumValue;
+            if (value > MaximumValue) value = MaximumValue;
+
+            
+            if (SliderValue == value) return;
+
+            
+            SliderValue = value;
+
+            
+            picSlider.Refresh();
+
+            
+            int tip_x = picSlider.Left + (int)ValueToX(SliderValue);
+            int tip_y = picSlider.Top;
+            tipValue.Show(SliderValue.ToString("0.00"), this, tip_x, tip_y, 3000);
+
+            
+            lblResult.Text = SliderValue.ToString("0.00");
+        }
+
+        
+    }
+}
